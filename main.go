fe6c786c35abcf4ef0ff04f2882b86a0994c1341
RiuSRoy <iamash20@gmail.com>
2023-07-05 23:03:14 +0530

    Queue setup

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+)
+
+type ConcurrentQueue struct {
+	queue []int32
+}
+
+func (q *ConcurrentQueue) Enqueue(item int32) {
+	q.queue = append(q.queue, item)
+}
+
+func (q *ConcurrentQueue) Dequeue() int32 {
+	if len(q.queue) == 0 {
+		panic("cannot dequeue from an empty queue")
+	}
+	item := q.queue[0]
+	q.queue = q.queue[1:]
+	return item
+}
+
+var wgE sync.WaitGroup
+
+func main() {
+
+	q1 := ConcurrentQueue{
+		queue: make([]int32, 0),
+	}
+
+	for i := 0; i < 100000; i++ {
+		wgE.Add(1) // everytime I spin up a new thread, I write this command
+		go func() {
+			q1.Enqueue(rand.Int31())
+			wgE.Done()
+		}()
+	}
+
+	wgE.Wait()                 // Will make sure we wait for all go routines to complete
+	fmt.Println(len(q1.queue)) // <1M because lot many threads are overwriting a the same location of the queue
+}
